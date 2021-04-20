e3a052fd5978c08642130358b1ad8f914e06fb6b
Shreedhar Natarajan <shreedharn@users.noreply.github.com>
2021-04-19 23:07:04 -0400

    Response returned

--- functions/generate_report/app.py
+++ functions/generate_report/app.py
@@ -6,9 +6,10 @@ iam_client = boto3.client('iam')
 
 def lambda_handler(event, context):
     # initial state to failed
-    response = {'state': 'failed', **event['report']}
+    response = {'state': 'failed', 'errorCode': None, **event['report']}
     cmd_response = iam_client.generate_credential_report()
     response['state'] = cmd_response['State']
+    return response
 
 
 
